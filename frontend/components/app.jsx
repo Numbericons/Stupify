252a93f05c3f7a1c8b9b3f3b381548902fe86b63
ZachO <zachjoliver@gmail.com>
2019-06-24 16:57:47 -0700

    About to try adding jquery

---
+++ frontend/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { AuthRoute } from '../util/route_util'
+// import { Route } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <header>
+        <h1>Stupify!</h1>
+        {/* <GreetingContainer /> */}
+    </header>
+
+    {/* <AuthRoute path="/login" component={ LoginFormContainer } /> */}
+    {/* <AuthRoute path="/signup" component={ SignupFormContainer} /> */}
+    {/* <Route exact path="/" component={ SearchContainer } /> */}
+  </div>
+);
+
+export default App;
--- frontend/stupify.jsx
+++ frontend/stupify.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
-// import Root from './components/root';
+import Root from './components/root';
 // import configureStore from './store/store';
 import * as SessionApiUtil from './util/session_api_util'
-
+import {createStore} from 'redux';
 
 document.addEventListener('DOMContentLoaded', () => {
 //   let store;  
@@ -19,14 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
 //     store = configureStore(preloadedState);
 //     delete window.currentUser;
 //   } else {
-  let store = configureStore();
+  // let store = createStore();
 //   }
-  window.dispatch = store.dispatch
-  window.getState = store.getState
+  // window.dispatch = store.dispatch
+  // window.getState = store.getState
   window.login = SessionApiUtil.login;
   window.signup = SessionApiUtil.signup;
   window.logout = SessionApiUtil.logout;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<h1>Welcome to stupify from entry</h1>, root);
+  // ReactDOM.render(<Root store={store} />, root);
 });
