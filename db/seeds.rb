9b9c4bd869892e87c1ce1357019a2008d3a951fb
ZachO <zachjoliver@gmail.com>
2019-06-27 09:29:25 -0700

    Add album model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# email = potterArr.map{|capStr| capStr.split("").reject{|char| char == " " }.join("").downcase}
 
 User.create(username: 'Demo User', email: 'demo', password: '123456')
+domains = [".com", '.co', '.edu', '.uk']
+symbols = "!@#$%^&*()-+=[];:,<>/?'|\`~ "
+
+50.times do
+    newUser = Faker::Movies::HarryPotter.character
+    emailName = newUser.split('').reject{ |ch| symbols.include?(ch) }.join("").downcase
+
+    location = Faker::Movies::HarryPotter.location
+    site = location.split('').reject{ |ch| symbols.include?(ch) }.join("").downcase
+
+    email = emailName + "@" + site + domains[rand(4)]
+    User.create(username: newUser, email: email, password: '123456')
+end
