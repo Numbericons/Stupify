b85aed92863e8a30c2b9b8c3d68717d5670d0774
ZachO <zachjoliver@gmail.com>
2019-06-27 14:30:53 -0700

    Rough navbar and splash background

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -7,18 +7,42 @@ const Greeting = ({ currentUser, logout, location }) => {
   if (location.pathname === "/login" || location.pathname === "/signup") {
     return null;
   }
-  const sessionLinks = () => (
-    <nav className="login-signup">
-      <Link to="/login">Login</Link>
-        &nbsp;or&nbsp;
-      <Link to="/signup">Sign up!</Link>
+
+  const navBar = () => (
+    <nav className='nav-bar'>
+      <h5>Spotify</h5>
+      <div>
+        <h6>Profile</h6>
+      </div>
     </nav>
+  )
+  const sessionLinks = () => (
+    <div>
+      <div className='splash-back'>
+        <div className='nav-bar'>
+          <div className='div-flex'>
+          
+            {/* <img className='nav-logo' src="https://png.pngtree.com/png-clipart/20190515/original/pngtree-spotify-social-media-icon-design-template-vector-png-image_3654774.jpg"/> */}
+            {/* <img className='nav-logo' src="https://i.ya-webdesign.com/images/spotify-logo-black-png-6.png"/> */}
+            <img className='nav-logo' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOkTSOGtXE8Co1jnD5Y7bFMWw26LH_wY1rPztQFE0mje6M9Nwz"/>
+            {/* <img className='nav-logo' src="https://image.flaticon.com/icons/png/512/121/121148.png"/> */}
+            <h1 className='nav-head-text'>Stupify</h1>
+          </div>
+          <nav className="login-signup">
+            <Link className='link-auth' to="/login">Login</Link>
+            <Link className='link-auth' to="/signup">Sign up</Link>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
   const personalGreeting = () => (
-    <hgroup className="header-cont">
-      <h6 className="header-name">Hi, {currentUser.username}!</h6>
-      <button className="header-button" onClick={logout}>Log Out</button>
-    </hgroup>
+    <div className='splash-back'>
+      <hgroup className="header-cont">
+        <h6 className="header-name">Hi, {currentUser.username}!</h6>
+        <button className="header-button" onClick={logout}>Log Out</button>
+      </hgroup>
+    </div>
   );
 
   return currentUser ? personalGreeting() : sessionLinks();
