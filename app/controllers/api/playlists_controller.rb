b95c512b856d0a3ee5e188a9857b4526b656cc1f
ZachO <zachjoliver@gmail.com>
2019-06-28 08:16:12 -0700

    Start json views about to push to heroku

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,16 +1,27 @@
 class Api::SessionsController < ApplicationController
     def show
+        @playlist = Playlist.find_by(id: params[:id])
+        render: show
     end
 
     def index
-    
+        @playlists = Playlist.all
+        render: index
     end
 
     def create
+        @playlist = Playlist.new(playlist_params)
+        if @playlist.save
+            render 'api/playlists/show'
+        else
+            render json: @playlist.errors.full_messages
+        end
     end
 
     def destroy
-    
+        @playlist = Playlist.find_by(id: params[:id])
+        Playlist.destroy(@playlist)
+        render "api/playlists/show"
     end
 
     def playlist_params
