8cbaff4455fc29882868f12ff4f1dfdded264e57
Numbericons <zachjoliver@gmail.com>
2021-07-12 20:14:01 -0700

    Have working sidebar and player separate components

--- frontend/components/playlist/playlist_index.jsx
+++ frontend/components/playlist/playlist_index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PlaylistIndexItem from './playlist_index_item';
 import CreatePlaylistFormContainer from './create_playlist_form_container';
-import SongShowAddItem from './song_show_add_item'
+import SongShowAddItem from './song_show_add_item';
 import Modal from '../modal/modal';
 
 class PlaylistIndex extends React.Component {
@@ -19,6 +19,7 @@ class PlaylistIndex extends React.Component {
 //       this.props.fetchPlaylist(this.props.match.params.playlistId);
 // //     }
 //   }
+
   play(e) {
     let audioObj3 = new Audio("https://stupify-dev.s3.us-west-1.amazonaws.com/Songs/ModChina100.mp3");
     audioObj3.play();
@@ -41,43 +42,10 @@ class PlaylistIndex extends React.Component {
         );
         });
     }
+
     return (
       <div>
         <div id='pl-show-big-cnt'>
-          <div className='sidebar-cnt'>
-            <div className='logo-box-pl'>
-              <img className='nav-logo-pl' src="https://stupify-dev.s3.us-west-1.amazonaws.com/logo.svg.png" />
-              <h2 className='nav-head-text-player'>Stupify</h2>
-            </div>
-            <br />
-            <br />
-            <div className='side-icon-link-cnt'>
-              <img className="pl-side-sm-icon" src={window.home} />
-              <Link id='pl-side-home-link' to="/playlists">Home</Link>
-            </div>
-            <div className='side-icon-link-cnt'>
-              <img className="pl-side-sm-icon" src={window.search} />
-              <Link id='pl-side-search-link' to="#">Search</Link>
-            </div>
-            <div className='side-icon-link-cnt'>
-              <img className="pl-side-sm-icon" src={window.library} />
-              <Link id='pl-side-library-link' to="/#">Your Library</Link>
-            </div>
-            <div id='side-recently-played'></div>
-            <div id='logout-curr-user-cont'>
-              <div id='logout-text-cnt'>
-                <p id='logout-text' onClick={this.props.logout}>Log Out</p>
-              </div>
-              <div className="or-side-cont">
-                <div className="curr-side-line-extd"></div>
-              </div>
-              <div id='curr-user-cnt'>
-                <img className="pl-side-sm-icon-user" src={window.user} />
-                <p id='curr-user-text'>{this.props.currUser.toLowerCase()}</p>
-                {/* <button className="logout-btn" onClick={this.props.logout}>Log Out</button> */}
-              </div>
-            </div>
-          </div>
           <Modal/>
           <div className='song-list-header'>
             <div className='pl-info-pane-cnt'>
@@ -94,32 +62,6 @@ class PlaylistIndex extends React.Component {
               <ul>
                   {dispSongs}
               </ul>
-            </div>
-            {/* <div className='pl-idx-background'></div> */}
-          </div>
-        </div>
-        <div id='player-cnt'>
-          <div id='player-song-info'>
-            <img id="player-heart-no" src={window.heartno} />
-            {/* <img id="player-heart-yes" src={window.heartyes} /> */}
-          </div>
-          <div id='player-control-ctn'>
-            <img id="player-random" src={window.random} />
-            <img id="player-back" src={window.back} />
-            <img id="player-play" src={window.playbtn} onClick={this.play}/>
-            <img id="player-forward" src={window.forward} />
-            <img id="player-repeat" src={window.repeat} />
-            {/* <div id='song-progress-cnt'> */}
-            <div className="song-progress-cont">
-              {/* <div>00:00</div> */}
-              <div className="song-progress"></div>
-              {/* <div>03:16</div> */}
-            </div>
-          </div>
-          <div id='player-volume-ctn'>
-            <img id="player-volume-up" src={window.volumeup} />
-            <div className="or-volume-cont">
-              <div className="or-side-line-extd"></div>
             </div>
           </div>
         </div>
--- frontend/components/playlist/playlist_show.jsx
+++ frontend/components/playlist/playlist_show.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SongShowItem from './song_show_item'
+import SongShowItem from './song_show_item';
 
 class PlaylistShow extends React.Component {
   componentDidMount() {
@@ -32,41 +32,6 @@ class PlaylistShow extends React.Component {
     return (
       <div>
         <div id='pl-show-big-cnt'>
-          <div className='sidebar-cnt'>
-            <div className='logo-box-pl'>
-              <img className='nav-logo-pl' src="https://stupify-dev.s3.us-west-1.amazonaws.com/logo.svg.png"/>
-              <h2 className='nav-head-text-player'>Stupify</h2>
-            </div>
-              <br/>
-                <br/>
-                <div className='side-icon-link-cnt'>
-                  <img className="pl-side-sm-icon" src={window.home} />
-                  <Link id='pl-side-home-link' to="/playlists">Home</Link>
-                </div>
-                <div className='side-icon-link-cnt'>
-                  <img className="pl-side-sm-icon" src={window.search} />
-                  <Link id='pl-side-search-link' to="#">Search</Link>
-                </div>
-                <div className='side-icon-link-cnt'>
-                  <img className="pl-side-sm-icon" src={window.library} />
-                  <Link id='pl-side-library-link' to="/#">Your Library</Link>
-                </div>
-                <div id='side-recently-played'></div>
-                <div id='logout-curr-user-cont'>
-                  <div id='logout-text-cnt'>
-                    <p id='logout-text' onClick={this.props.logout}>Log Out</p>
-                  </div>
-                  <div className="or-side-cont">
-                    <div className="curr-side-line-extd"></div>
-                  </div>
-                  <div id='curr-user-cnt'>
-                  <img className="pl-side-sm-icon-user" src={window.user} />
-                    <p id='curr-user-text'>{this.props.currUser.toLowerCase()}</p>
-                    {/* <button className="logout-btn" onClick={this.props.logout}>Log Out</button> */}
-                  </div>
-                </div>
-          </div>
-          {/* <div className='pl-album-cnt'></div> */}
           <div className='song-list-header'>
             <div className='pl-info-pane-cnt'>
               <div id='pl-info-cnt'>
@@ -89,31 +54,7 @@ class PlaylistShow extends React.Component {
               </div>
             </div>
           </div>
-          <div id='player-cnt'>
-            <div id='player-song-info'>
-              <img id="player-heart-no" src={window.heartno} />
-              {/* <img id="player-heart-yes" src={window.heartyes} /> */}
-            </div>
-            <div id='player-control-ctn'>
-              <img id="player-random" src={window.random} />
-              <img id="player-back" src={window.back} />
-              <img id="player-play" src={window.playbtn} />
-              <img id="player-forward" src={window.forward} />
-              <img id="player-repeat" src={window.repeat} />
-              {/* <div id='song-progress-cnt'> */}
-                <div className="song-progress-cont">
-                    {/* <div>00:00</div> */}
-                    <div className="song-progress"></div>
-                    {/* <div>03:16</div> */}
-                </div>
-            </div>
-            <div id='player-volume-ctn'>
-              <img id="player-volume-up" src={window.volumeup} />
-              <div className="or-volume-cont">
-                  <div className="or-side-line-extd"></div>
-              </div>
-            </div>
-          </div>
+          {/* <Player/> */}
         </div>
       </div>
     );
