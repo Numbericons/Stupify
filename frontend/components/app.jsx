331d7d62d91c606d3f264d7242d8d23a24c602a2
Numbericons <zachjoliver@gmail.com>
2021-07-12 20:13:22 -0700

    Add stand alone sidebar and player to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,28 +4,23 @@ import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 import PlaylistIndexContainer from './playlist/playlist_index_container.jsx';
 import PlaylistShowContainer from './playlist/playlist_show_container';
+import PlayerContainer from './player/player_container';
+import SidebarContainer from './sidebar/sidebar_container';
 import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-
-// const Auth = () => {
-// }
 
 const App = () => {
   return (
     <div>
-    {/* <header>
-      <div className='stupify-head'>
-        <img className='logo' src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.iconfinder.com%2Fdata%2Ficons%2Fsimple-icons%2F512%2Fspotify-512-black.png&f=1"/>
-        <h1 className='stupify-head-text'>Stupify</h1>
-      </div>
-    </header> */}
-    <Switch>
-      <AuthRoute exact path="/" component={GreetingContainer}/>
-      <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      <ProtectedRoute exact path="/playlists" component={PlaylistIndexContainer} />
-      <ProtectedRoute path="/playlists/:playlistId" component={PlaylistShowContainer}/>
-    </Switch>
+      <ProtectedRoute path="/" component={SidebarContainer} />
+      <Switch>
+        <AuthRoute exact path="/" component={GreetingContainer}/>
+        <AuthRoute exact path="/login" component={LogInFormContainer} />
+        <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+        <ProtectedRoute exact path="/playlists" component={PlaylistIndexContainer} />
+        <ProtectedRoute path="/playlists/:playlistId" component={PlaylistShowContainer}/>
+      </Switch>
+      <ProtectedRoute path="/" component={PlayerContainer} />
     </div>
   )
 };
